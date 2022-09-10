fd57bbeaccd02736e21ffec72ab5b0ea51fc7601
Samuel <samakin721@gmail.com>
2022-09-09 17:03:36 -0700

    hii

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,21 +13,21 @@ int main(void)
 
 	char a = 'a';
 
-
 	while (a <= 'z')
 
 	{
 
 		putchar(a);
 		a++;
-	
+
 	}
 
 
 	a = 'A';
 	while (a <= 'Z')
-	{	
-		putchar(a);									a++;
+	{
+ 		putchar(a);									
+		a++;
 	}
 
 	putchar('\n');
