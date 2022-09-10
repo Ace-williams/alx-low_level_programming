3f5670810377659efa540f6f6eea99c3debe9ecf
Samuel <samakin721@gmail.com>
2022-09-09 17:46:18 -0700

    gggdd

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	char alp;
+
+	for (alp = 'z'; alp >= 'a'; alp--)
+		putchar(alp);
+
+	putchar('\n');
+	return (0);
+
+}
