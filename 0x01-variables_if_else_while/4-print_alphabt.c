62d1221330baf208cd430e9fe9b64ec8137b638d
Samuel <samakin721@gmail.com>
2022-09-09 17:23:48 -0700

    alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+
+		if (a != 'e' && a != 'q')
+			putchar(a);
+	putchar('\n');
+
+	return (0);
+
+}
