086464bd3327b0230971be0897c82ae75dc17123
Samuel <samakin721@gmail.com>
2022-09-09 17:42:04 -0700

    jjj

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - printing numbers
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+		int d;
+
+	for (d = 0; d < 10; d++)
+		putchar((d % 10) + '0');
+	putchar('\n');
+
+	return (0);
+
+}
